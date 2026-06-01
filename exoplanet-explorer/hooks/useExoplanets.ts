d97e92dd5871277d614a2577b33898f94a43490b
sqacetimee <jenningsjoshua72@gmail.com>
2026-06-01 05:03:03 -0400

    Remove old exoplanet-explorer folder

--- exoplanet-explorer/hooks/useExoplanets.ts
+++
@@ -1,4 +0,0 @@
-"use client";
-
-export { usePlanets as useExoplanets } from "@/components/providers/PlanetsProvider";
-export type { PlanetsContextValue } from "@/components/providers/PlanetsProvider";
